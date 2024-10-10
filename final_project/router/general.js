0eecac9bd335dc79cfe6f03bd076ff056d06fa78
Yifei Yan <yifeiyan@umich.edu>
2024-10-09 23:09:07 -0400

    General task 1 and 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,13 +12,20 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.status(200).json({message: JSON.stringify(books, null, 4)});
+  return res.status(200).json({message: JSON.stringify(books)});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = parseInt(req.params.isbn);
+  console.log("Books.length is " + books.length);
+  if (isbn in books){
+    let book = books[req.params.isbn];
+    return res.status(200).json({message: JSON.stringify(book)});
+  }
+  else {
+    return res.status(300).json({message: `Book not found with ISBN ${isbn}`});
+  }
  });
   
 // Get book details based on author
