06ad43d2e9652429f180bb9c9a342ac33b8e4c38
Yifei Yan <yifeiyan@umich.edu>
2024-10-09 23:38:31 -0400

    General task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,8 +30,22 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let matches = [];
+  let keys = Object.keys(books);
+  keys.forEach((key, i) => {
+    book = books[key];
+    if (author === book.author) {
+        matches.push(book);
+    }
+  })
+
+  if (matches.length > 0) {
+    return res.status(200).json({message: JSON.stringify(matches)})
+  }
+  else {
+    return res.status(300).json({message: `No book found by author ${author}`});
+  }
 });
 
 // Get all books based on title
